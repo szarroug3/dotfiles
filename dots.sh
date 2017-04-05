e10f47b27481c159e1f34bd9b609c9e15b5c72d5
Samreen Zarroug <szarroug3@gmail.com>
2017-04-05 15:17:17 -0500

    initializing with the files i have so far

---
+++ dots.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+#.....................
+# Look
+#.....................
+
+# xinitrc
+ln -s ${BASEDIR}/xinitrc $HOME/.xinitrc
+
+# Xresources
+ln -s ${BASEDIR}/Xresources $HOME/.Xresources
+
+# Xresources.d
+ln -s ${BASEDIR}/Xresources.d $HOME/.Xresources.d
+
+# i3
+ln -s ${BASEDIR}/i3 $HOME/.config/i3
+
+# compton
+ln -s ${BASEDIR}/compton.conf $HOME/.config/compton.conf
+
+#.....................
+# Terminal
+#.....................
+
+# zsh
+ln -s ${BASEDIR}/zsh/zprofile $HOME/.zprofile
+ln -s ${BASEDIR}/zsh/zshrc $HOME/.zshrc
+
+#.....................
+# Apps
+#.....................
+
+# bin
+ln -s ${BASEDIR}/bin $HOME/bin
